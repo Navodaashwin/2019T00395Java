b054e2df9a7a5b2c9368ac8b5e97a800aad1cc6e
Navodaashwin <2019t00395@stu.com.ac.lk>
2021-10-06 07:55:59 +0530

    Added if examples to main

--- IA2211Lesson2/src/com/company/Main.java
+++ IA2211Lesson2/src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
+    static String name="Ashwin";
+
     public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Name " + name);
 	// write your code here
     }
 }
