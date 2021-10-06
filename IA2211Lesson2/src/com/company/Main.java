b509a4f578f469ff06d1138d950430d556b6184d
Navodaashwin <2019t00395@stu.com.ac.lk>
2021-10-06 11:09:59 +0530

    Added if examples to main

--- IA2211Lesson2/src/com/company/Main.java
+++ IA2211Lesson2/src/com/company/Main.java
@@ -3,10 +3,29 @@ package com.company;
 public class Main {
 
     static String name="Ashwin";
-
+    public static final int serialNum = 10; //defind constent
+    boolean isActive = false;
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Name " + name);
-	// write your code here
+
+        String myname ="Navoda" + " " + "Ashwin";
+        System.out.println(myname);
+
+        int valA = 10;
+        double valB = (double) valA;
+
+        float valC = valA / (float) 2.5;
+        System.out.println("valC = " + valC);
+        int age = 28;
+        if(age<18){
+            System.out.println("Not eligible");
+        }
+        else if(age>88){
+            System.out.println("Too lod");
+        }
+        else{
+            System.out.println("Allowed");
+        }
     }
 }
