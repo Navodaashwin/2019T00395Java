03589d8bdc0e46289fa0e42c034a9c2a6579cc51
Navodaashwin <2019t00395@stu.com.ac.lk>
2021-12-24 21:34:34 +0530

    init

---
+++ IA2211Lesson3/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int x=2;
+        switch (x){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("error");
+        }
+
+        }
+}
