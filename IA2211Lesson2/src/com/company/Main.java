98d6784675b759efac959a2e5634c7631f5fb518
Navodaashwin <2019t00395@stu.com.ac.lk>
2021-12-24 19:14:08 +0530

    init

--- IA2211Lesson2/src/com/company/Main.java
+++ IA2211Lesson2/src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     static String name="Ashwin";
-    public static final int serialNum = 10; //defind constent
+    static final int serialNum = 10; //defind constent
     boolean isActive = false;
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Name " + name);
-
+//comment
         String myname ="Navoda" + " " + "Ashwin";
         System.out.println(myname);
 
@@ -19,7 +19,7 @@ public class Main {
         System.out.println("valC = " + valC);
         int age = 28;
         if(age<18){
-            System.out.println("Not eligible");
+            System.out.println("Nosdadasdasdast eligible");
         }
         else if(age>88){
             System.out.println("Too lod");
